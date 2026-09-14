eed1c49dc1e13443f7323688622cb0de33a807e4
Kostiantyn Shevchuk <constantine.shevchuk@gmail.com>
2026-09-14 23:47:14 +0300

    final2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
-import Hero from '@/components/Hero/Hero';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+import Catalog from '@/components/Catalog/Catalog';
+import { fetchCars, fetchCarsFilters } from '@/services/carService';
 
-export default function HomePage() {
-  return <Hero />;
+export default async function CatalogPage() {
+  const queryClient = new QueryClient();
+
+  await Promise.all([
+    queryClient.infiniteQuery({
+      queryKey: ['cars', {}],
+      queryFn: ({ pageParam }) =>
+        fetchCars({
+          page: pageParam,
+        }),
+      initialPageParam: 1,
+    }),
+
+    queryClient.query({
+      queryKey: ['carsFilters'],
+      queryFn: fetchCarsFilters,
+    }),
+  ]);
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Catalog />
+    </HydrationBoundary>
+  );
 }
--- src/components/TanStackProvider/TanStackProvider.tsx
+++ src/components/TanStackProvider/TanStackProvider.tsx
@@ -9,7 +9,16 @@ interface TanStackProviderProps {
 }
 
 export default function TanStackProvider({ children }: TanStackProviderProps) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
