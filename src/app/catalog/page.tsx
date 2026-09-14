e39f53ee37c1f5b5c0b1136cbd58dbee730546d6
Kostiantyn Shevchuk <constantine.shevchuk@gmail.com>
2026-09-14 23:54:14 +0300

    final3

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,5 +1,33 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
 import Catalog from '@/components/Catalog/Catalog';
+import { fetchCars, fetchCarsFilters } from '@/services/carService';
 
-export default function CatalogPage() {
-  return <Catalog />;
+export default async function CatalogPage() {
+  const queryClient = new QueryClient();
+
+  await Promise.all([
+    queryClient.infiniteQuery({
+      queryKey: ['cars', {}],
+      queryFn: ({ pageParam }) =>
+        fetchCars({
+          page: pageParam,
+        }),
+      initialPageParam: 1,
+    }),
+
+    queryClient.query({
+      queryKey: ['carsFilters'],
+      queryFn: fetchCarsFilters,
+    }),
+  ]);
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Catalog />
+    </HydrationBoundary>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,5 @@
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from '@tanstack/react-query';
-import Catalog from '@/components/Catalog/Catalog';
-import { fetchCars, fetchCarsFilters } from '@/services/carService';
+import Hero from '@/components/Hero/Hero';
 
-export default async function CatalogPage() {
-  const queryClient = new QueryClient();
-
-  await Promise.all([
-    queryClient.infiniteQuery({
-      queryKey: ['cars', {}],
-      queryFn: ({ pageParam }) =>
-        fetchCars({
-          page: pageParam,
-        }),
-      initialPageParam: 1,
-    }),
-
-    queryClient.query({
-      queryKey: ['carsFilters'],
-      queryFn: fetchCarsFilters,
-    }),
-  ]);
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Catalog />
-    </HydrationBoundary>
-  );
+export default function HomePage() {
+  return <Hero />;
 }
